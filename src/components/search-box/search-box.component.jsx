3b1f737f43d6a79bcd4bcba5b63f35046984eeb6
Francisco Antonio Duran Ramirez <ing.antonio.duran@gmail.com>
2023-04-16 16:51:13 -0600

    [ search-box.component class component to functional component ]

--- src/components/search-box/search-box.component.jsx
+++ src/components/search-box/search-box.component.jsx
@@ -1,17 +1,26 @@
-import { Component } from "react";
-import './search-box.styles.css';
+//import { Component } from "react";
+import "./search-box.styles.css";
 
-class SearchBox extends Component {
-    render () {
-        return (
-            <input
-            className={`search-box ${this.props.className}`}
-            type='search'
-            placeholder={this.props.placeHolder}
-            onChange= {this.props.onChangeHandler}
-          />
-        )
-    }
-}
+const SearchBox = ({ className, placeHolder, onChangeHandler }) => (
+  <input
+    className={`search-box ${className}`}
+    type="search"
+    placeholder={placeHolder}
+    onChange={onChangeHandler}
+  />
+);
+
+// class SearchBox extends Component {
+//     render () {
+//         return (
+//             <input
+//             className={`search-box ${this.props.className}`}
+//             type='search'
+//             placeholder={this.props.placeHolder}
+//             onChange= {this.props.onChangeHandler}
+//           />
+//         )
+//     }
+// }
 
 export default SearchBox;
